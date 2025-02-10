930874ecf3b329c9d3c7980ccfab43f357de809f
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2025-02-10 21:20:56 +0100

    Added new columns in table of users

--- BookServices.SqlDatabase/dbo/Tables/Users.sql
+++ BookServices.SqlDatabase/dbo/Tables/Users.sql
@@ -1,7 +1,10 @@
 ﻿CREATE TABLE [dbo].[Users] (
     [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [LastName] NVARCHAR (50) NOT NULL,
     [Email]           NVARCHAR (100) NOT NULL,
     [HashedPassword]  NVARCHAR (200) NOT NULL,
+    [Type] NVARCHAR(20) NOT NULL,
     [RegisterDate]    DATETIMEOFFSET  NOT NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );

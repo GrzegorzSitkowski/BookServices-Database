0c97c8812dd8b9f825b8537a00dac230560d6a50
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-04-22 16:59:26 +0200

    Created table of Reservations

---
+++ BookServices.SqlDatabase/dbo/Tables/Reservations.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Reservations]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[Name] VARCHAR(50) NOT NULL,
+	[UserName] VARCHAR(100) NOT NULL,
+	[WorkerName] VARCHAR(50) NOT NULL,
+	[VenueName] VARCHAR(100) NOT NULL,
+	[UserId] INT NOT NULL,
+	[VenueId] INT NOT NULL,
+	[WorkerId] INT NOT NULL,
+	[DateFrom] DATETIMEOFFSET NOT NULL,
+	[DateTo] DATETIMEOFFSET NOT NULL,
+	[Status] VARCHAR(20) NOT NULL,
+	CONSTRAINT [PK_Reservations] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Reservations_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_Reservations_Venues] FOREIGN KEY ([VenueId]) REFERENCES [dbo].[Venues] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_Reservations_Workers] FOREIGN KEY ([WorkerId]) REFERENCES [dbo].[Workers] ([Id])
+)
